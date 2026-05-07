98a85f2fb26f22c24bb902de1d16b34d71da5fd5
lahh29 <capacitacionvinoqro@gmail.com>
2026-05-07 16:40:25 -0600

    update

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -49,7 +49,9 @@ const DrawerContent = React.forwardRef<
       {...props}
     >
       <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-border" />
-      {children}
+      <div className="flex-1 overflow-y-auto overscroll-contain">
+        {children}
+      </div>
     </DrawerPrimitive.Content>
   </DrawerPortal>
 ))
